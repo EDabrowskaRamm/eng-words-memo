570d3de47e03fd9ba18331880d55df290fa19610
Edyta Dabrowska-Ramm <editt.dabrowska@gmail.com>
2017-12-18 10:29:28 +0100

    add nameInput to register

--- src/components/register/Register.vue
+++ src/components/register/Register.vue
@@ -1,13 +1,32 @@
 <template lang="html">
   <div class="container">
     <p>register!!</p>
+    <form>
+      <div class="form-group">
+        <label for="firstName">First name
+          <NameInput/>
+        </label>
+        <label for="firstName">Last name
+          <NameInput/>
+        </label>
+      </div>
+    </form>
+
   </div>
 </template>
 
 <script>
+import NameInput from '../../shared/NameInput'
+
 export default {
+  components: {
+    NameInput
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+  form {
+    text-align: left;
+  }
 </style>
---
+++ src/shared/NameInput.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <input type="text" class="form-control" id="nameInput" aria-describedby="emailHelp" placeholder="First name">
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+</style>
