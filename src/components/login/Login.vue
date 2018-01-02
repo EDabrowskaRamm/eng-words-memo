3dd74e9b4daf12c441751dfe128e8dacbf9f6ffa
Edyta Dabrowska-Ramm <editt.dabrowska@gmail.com>
2018-01-02 13:22:55 +0100

    login form styles done

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -1,13 +1,67 @@
 <template lang="html">
   <div class="container">
-    <p>login!!</p>
+    <form>
+      <h3>Log in</h3>
+      <div class="form-group">
+        <i class="fa fa-user" aria-hidden="true"></i>
+        <NameInput/>
+      </div>
+      <div class="form-group">
+        <i class="fa fa-lock" aria-hidden="true"></i>
+        <PassInput/>
+      </div>
+    </form>
   </div>
 </template>
 
 <script>
+import NameInput from '../../shared/NameInput'
+import PassInput from '../../shared/PassInput'
+// import EmailInput from '../../shared/EmailInput'
+
 export default {
+  components: {
+    NameInput,
+    PassInput
+    // EmailInput
+  }
 }
 </script>
 
-<style lang="css">
+<style lang="scss" scoped>
+.container {
+  form {
+    display: flex;
+    flex-direction: column;
+    text-align: left;
+    background-color: white;
+    padding: 1rem;
+    margin: 1rem 0;
+    box-shadow: 10px 10px 5px -5px rgba(0,0,0,0.26);
+    h3 {
+      text-align: center;
+      margin-bottom: 1.5rem;
+    }
+    .form-group {
+      margin-bottom: 1.5rem;
+      .fa {
+        font-size: 1.4rem;
+        padding-right: .75rem;
+      }
+    }
+  }
+
+
+  //mobile
+  @media(min-width: 767px) {
+    .container {
+      display: flex;
+      justify-content: center;
+      form {
+        width: 481px;
+      }
+    }
+  }
+
+}
 </style>
